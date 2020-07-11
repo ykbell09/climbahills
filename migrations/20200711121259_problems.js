d690978b54fd555b0922947aeafb33d4844b27cb
Yvette Bell <ykbell09@gmail.com>
2020-07-11 12:59:05 -0700

    updates to problems db - knex migration, seed and UI

---
+++ migrations/20200711121259_problems.js
@@ -0,0 +1,20 @@
+
+exports.up = async knex => {
+    await knex.schema.dropTableIfExists('problems');
+    await knex.schema.createTable('problems', table => {
+        table.increments('id').primary();
+        table.string('name').unique();
+        table.string('fa', 30);
+        table.string('setter', 30).notNullable();
+        table.integer('grade');
+        table.string('plus_minus', 1);
+        table.timestamp('date_set').notNullable().defaultTo(knex.fn.now());
+        table.timestamp('date_removed');
+        table.string('tape_color', 10).notNullable().defaultTo('none');
+        table.string('notes');
+    });
+};
+
+exports.down = async knex => {
+    await knex.schema.dropTableIfExists('problems');
+};
